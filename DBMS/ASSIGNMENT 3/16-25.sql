601c71b55e8ea48b01f41a5d88cc3017bc61e7c8
BEAST <110054039+BEASTgg@users.noreply.github.com>
2023-08-24 07:57:11 -0700

    Update 16-25.sql

--- DBMS/ASSIGNMENT 3/16-25.sql
+++ DBMS/ASSIGNMENT 3/16-25.sql
@@ -16,7 +16,7 @@ SELECT AVG(commission) AS average_commission FROM employee;
 
 -- 20. Find the average salaries of the employees department wise
 
-SELECT dept, AVG(salary) AS average_salary FROM employee GROUP BY dept;
+SELECT DISTINCT dept, AVG(salary) AS average_salary FROM employee GROUP BY dept;
 
 -- 21. Find the sum of salary of different job_type according to different departments
 
@@ -24,7 +24,7 @@ SELECT DISTINCT dept, job_type, SUM(salary) AS total_salary FROM employee GROUP 
 
 -- 22. Find the department name and average salaries of those departments whose average salary is greater than 40000
 
-SELECT dept, AVG(salary) AS average_salary FROM employee GROUP BY dept HAVING AVG(salary) > 40000;
+SELECT DISTINCT dept, AVG(salary) AS average_salary FROM employee GROUP BY dept HAVING AVG(salary) > 40000;
 
 -- 23. Find the department name and maximum salaries of those departments whose maximum salary is greater than 55000
 
