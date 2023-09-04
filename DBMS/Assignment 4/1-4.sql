16f3d87ed3ecadcac07c92ee70137debdf4a5d6b
BEAST <110054039+BEASTgg@users.noreply.github.com>
2023-09-04 07:35:29 -0700

    Update 1-4.sql

--- DBMS/Assignment 4/1-4.sql
+++ DBMS/Assignment 4/1-4.sql
@@ -4,29 +4,29 @@ SELECT UPPER('hello world!'), LOWER('HELLO WORLD!');
 
 -- 2. Show the use of concat, instr and length function
 
-SELECT CONCAT('Hello' , 'world');
+SELECT CONCAT('Hello' , 'world') FROM DUAL;
 
-SELECT INSTR('Hello World', 'r');
+SELECT INSTR('Hello World', 'r') FROM DUAL;
 
-SELECT LENGTH('Hello world!');
+SELECT LENGTH('Hello world!') FROM DUAL;
 
 -- 3. Show the use of the following functions on numeric values:
 
 -- a. Sqrt()
 
-SELECT sqrt(25);
+SELECT sqrt(25) FROM DUAL;
 
 -- b. Power()
 
-SELECT POWER(5,2);
+SELECT POWER(5,2) FROM DUAL;
 
 -- c. Ceil()
 
-SELECT CEIL(12.534);
+SELECT CEIL(12.534) FROM DUAL;
 
 -- d. Substr()
 
-SELECT SUBSTR('Hellow World', 3, 3);
+SELECT SUBSTR('Hellow World', 3, 3) FROM DUAL;
 
 -- e. Max()
 
@@ -38,7 +38,7 @@ SELECT MIN(salary) FROM employee;
 
 -- g. Round()
 
-SELECT ROUND(24.674);
+SELECT ROUND(24.674) FROM DUAL;
 
 -- h. avg()
 
@@ -50,22 +50,22 @@ SELECT COUNT(dept) FROM employee;
 
 -- j. Exp()
 
-SELECT EXP(5);
+SELECT EXP(5) FROM DUAL;
 
 -- k. mod()
 
-SELECT MOD(8, 4);
+SELECT MOD(8, 4) FROM DUAL;
 
 -- 4. Solve the following queries
 
 -- a. Find the ceiling and floor value of 14.887.
 
-SELECT CEIL(14.887), FLOOR(14.887);
+SELECT CEIL(14.887), FLOOR(14.887) FROM DUAL;
 
 -- b. Find out the round-off 17.49989.
 
-SELECT ROUND(17.49989);
+SELECT ROUND(17.49989) FROM DUAL;
 
 -- c. Calculate 8^7.
 
-SELECT POWER(8, 7);
+SELECT POWER(8, 7) FROM DUAL;
