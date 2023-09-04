b2c7c69f144ff16f63509f638a498349d3ce538f
BEAST <110054039+BEASTgg@users.noreply.github.com>
2023-09-04 10:26:31 -0700

    Update 5-14.sql

--- DBMS/Assignment 4/5-14.sql
+++ DBMS/Assignment 4/5-14.sql
@@ -121,7 +121,7 @@ RIGHT OUTER JOIN department ON employee.dept = department.d_name;
 SELECT *FROM employee
 FULL OUTER JOIN department ON employee.dept = department.d_name;
 
--- 13. Write a query to find the address.  -> Confusion !
+-- 13. Write a query to find the address.  -> incomplete QS
 
 SELECT locations.location_id, locations.street_address, locations.city, locations.state_province, countries.country_name FROM locations
 NATURAL JOIN countries ON locations.country_id = countries.country_id;
