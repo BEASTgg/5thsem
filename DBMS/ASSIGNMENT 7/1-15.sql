94edad37361187af3c05aa0f8dfae2e67fad8535
BEAST <110054039+BEASTgg@users.noreply.github.com>
2023-09-27 08:50:02 -0700

    Update 1-15.sql

--- DBMS/ASSIGNMENT 7/1-15.sql
+++ DBMS/ASSIGNMENT 7/1-15.sql
@@ -23,43 +23,57 @@ VALUES
 
 -- 2. Display the previous and current job_types of all the employees.
 
-
+SELECT employee.F_name, employee.job_type AS PrevJob, Job_History.job_type CurrJob FROM employee
+JOIN job_history ON employee.Emp_id = Job_History.Emp_id;
 
 -- 3. Display the previous and current department and job_types of all the employees.
 
-
+SELECT employee.F_name, employee.dept AS PrevDept, Job_History.D_name AS CurrDept, Job_History.job_type FROM employee
+JOIN job_history ON employee.Emp_id = Job_History.Emp_id;
 
 -- 4. Display the employee id and job_types of the employees who currently have a job title that they held previously.
 
-
+SELECT employee.Emp_id, Job_history.job_type FROM employee
+JOIN job_history ON employee.Emp_id = Job_History.Emp_id
+WHERE employee.job_type = Job_History.job_type;
 
 -- 5. Find the name of those employees who have not changed their jobs once.
 
-
+SELECT CONCAT(employee.F_name, ' ' , employee.L_name) AS NameOfEmployee FROM employee
+JOIN job_history ON employee.Emp_id = Job_History.Emp_id
+WHERE employee.job_type = Job_History.job_type;
 
 -- 6. Find the names of the employees who earn more than Chitra.
 
-
+SELECT CONCAT(F_name, ' ', L_name) AS NameOfEmployee FROM employee
+WHERE salary > (SELECT salary FROM employee WHERE F_name = 'Chitra');
 
 -- 7. Find the details of those employees who have the same job_type as of emp_id 7.
 
-
+SELECT *FROM employee
+WHERE job_type = (SELECT job_type FROM employee WHERE Emp_id = 7);
 
 -- 8. Find the details of the employees whose job_type is same as that of emp_id 3 and whose salary is greater than that of emp_id 7.
 
-
+SELECT *FROM employee
+WHERE job_type = (SELECT job_type FROM employee WHERE Emp_id = 3) AND salary > (SELECT salary FROM employee WHERE Emp_id = 7);
 
 -- 9. Display l_name, job_type and the salary of the employees whose salary is equal to the minimum salary.
 
-
+SELECT L_name, job_type, salary FROM employee
+WHERE salary = (SELECT MIN(salary) FROM employee);
 
 -- 10. Find the job_type with lowest average salary.
 
-
+SELECT job_type, AVG(salary) AS average_salary FROM employee
+GROUP BY job_type
+ORDER BY average_salary ASC
+LIMIT 1;
 
 -- 11. Display all the departments that have a minimum salary greater than that of ‘Sales’ department.
 
-
+SELECT dept FROM employee GROUP BY dept
+HAVING MIN(salary) > (SELECT MIN(salary) FROM employee WHERE dept = 'Sales');
 
 -- 12. Find the employees who earn the same salary for each department.
 
@@ -67,11 +81,13 @@ VALUES
 
 -- 13. Display the employees who are not engineers and whose salary is less than that of any engineer.
 
-
+SELECT CONCAT(F_name, ' ', L_name) AS NameOfEmployee FROM employee
+WHERE job_type != 'Engineer' AND salary < (SELECT MIN(salary) FROM employee WHERE job_type = 'Engineer');
 
 -- 14. Display the employees whose salary is less than the salary of all employees with a job_type ‘Clerk’ and whose job_type is not ‘Clerk’.
 
-
+SELECT CONCAT(F_name, ' ', L_name) AS NameOfEmployee FROM employee
+WHERE salary < (SELECT MIN(salary) FROM employee WHERE job_type = 'Clerk') AND job_type != 'Clerk';
 
 -- 15. Consider the following database of students enrollment in courses and books adopted for each course.
 
@@ -98,5 +114,4 @@ VALUES
 
 
 -- v) List any department that has all its adopted books published by a specific publisher.
-
 
