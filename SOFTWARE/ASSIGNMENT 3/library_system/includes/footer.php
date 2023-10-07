353cebf2d586c69b4c4868f0e9b8cf364b4ea4c8
BEAST <110054039+BEASTgg@users.noreply.github.com>
2023-10-06 22:02:08 -0700

    Delete SOFTWARE/ASSIGNMENT 3 directory

--- SOFTWARE/ASSIGNMENT 3/library_system/includes/footer.php
+++
@@ -1,6 +0,0 @@
-</main>
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> Library System</p>
-    </footer>
-</body>
-</html>
--- SOFTWARE/ASSIGNMENT 3/library_system/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Location: index.php"); // Redirect to the home page after logout
-exit();
-?>
